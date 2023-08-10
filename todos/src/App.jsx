031ff6dc56267178446c3c58d2942465af9b0015
sumns <sks23info@gmail.com>
2023-08-10 12:02:16 +0530

    select countries and its shows the cities name

--- todos/src/App.jsx
+++ todos/src/App.jsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React,{useState} from "react";
 import "./App.css";
 
+const countries = [ 
+  {
+    name : "India" , value : "In" , cities : ['Delhi', 'Mumbai']
+  },
+  {
+    name : "Pak" , value : "pk" , cities : ['Lahor', 'Karachi']
+  },
+  {
+    name : "Bangladesh" , value : "Bg" , cities : ['Dhaka', 'Chittagong']
+  }
+]
+
 const App = () => {
+  const[country , setCountry] = useState({name:"" , value: "" , cities:[]});
   return (
     <div>
+      <select value={country} onChange={(e)=> {
+        console.log(e.target.value)
+        setCountry(e.target.value)
+      } }>
+        {
+        countries.map((item, index)=>{
+          return <option value={index}>{item.name}</option>
+          
+        })}
+      </select>
 
+      <select value={country}>
+        {countries[country].cities.map((item, index)=>{
+          return <option value={index}>{item}</option>
+          
+        })}
+      </select>
     </div>
   );
 };
